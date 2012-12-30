2c49da481239e4d149faebca621015e62e28a2d8
Ryan Prichard <ryan.prichard@gmail.com>
2012-12-30 13:14:51 -0800

    Fix the packaging steps at the end of master-build-script.sh.

--- master-build-script.sh
+++ master-build-script.sh
@@ -7,8 +7,10 @@ SCRIPT_SRC=$(cd $(dirname $0) && /bin/pwd)
 
 package() {
     # xz tarballs do not work on OpenSUSE 11.2, so use bz2.
+    cd install
     rm -f $1.tar.bz2
     tar cfj $1.tar.bz2 $1
+    cd ..
 }
 
 $SCRIPT_SRC/get-packages.sh
@@ -17,7 +19,6 @@ $SCRIPT_SRC/build-gcc46.sh
 $SCRIPT_SRC/build-gcc47.sh
 $SCRIPT_SRC/extract-gcc-libs.sh 4.6.3
 $SCRIPT_SRC/extract-gcc-libs.sh 4.7.2
-$SCRIPT_SRC/package.sh
 package clang-3.2-${ARCH}-linux
-package gcc-libs-4.6-3-${ARCH}-linux
+package gcc-libs-4.6.3-${ARCH}-linux
 package gcc-libs-4.7.2-${ARCH}-linux
